1c068f4db84a495efc0bf8ac2e2e1d46ea261547
jvras <jvras@cin.ufpe.br>
2023-10-02 08:27:55 -0300

    WiP: Correção ainda necessaria em alguns casos

--- main.py
+++ main.py
@@ -8,20 +8,6 @@ canal_alvo_id = 1158343397279543327
 async def on_ready():
     print('Logado como {0.user}'.format(cliente_discord))
 
-#versão mais estavel ate agora (que consegue tratar mais ou menos o caso de isabelle)
-@cliente_discord.event
-async def on_message(mensagem):
-    if mensagem.channel.id == canal_alvo_id:
-        linhas = mensagem.content.split('\n')
-        if len(linhas) == 4:
-            primeira_linha = linhas[0]
-            if primeira_linha.startswith('- **Hj estou') or primeira_linha.startswith('Hj estou:') or primeira_linha.startswith('- Hj estou:'):
-                emoji = primeira_linha.split(':')[-1].strip()
-                if emoji.startswith('**'):
-                    emoji = emoji[2:]
-                await mensagem.channel.send(f'O emoji é {emoji}')
-                
-# melhor versão porem não consigo tratar o erro de quando a mensagem começa assim: - **Hj estou:** :woman_detective:  (o caso de isabelle)
 @cliente_discord.event
 async def on_message(mensagem):
     if mensagem.channel.id == canal_alvo_id:
@@ -34,8 +20,9 @@ async def on_message(mensagem):
                     emoji = partes[1].strip()
                     if emoji.startswith('**'):
                         emoji = emoji[2:]
-                    if ' ' in emoji:
-                        emoji = emoji.split(' ')[0]
+                    # FIXME: essa parte de remover o texto que vem com o emoji ta sem funcionar direito ACABA REMOVENDO O EMOJI É DEIXANDO A MENSAGEM EM BRANCO EM ALGUNS CASOS (CASO DE ISABELLE)
+                    # if ' ' in emoji:
+                    #     emoji = emoji.split(' ')[0]
                     await mensagem.channel.send(f'O emoji é {emoji}')
 # Inicialização do cliente do Discord com o token de autenticação
 cliente_discord.run('')
