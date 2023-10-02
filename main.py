c9c19550b2b66edddf3f5483b166a31238a5ea6d
jvras58 <jvras@cin.ufpe.br>
2023-10-02 19:40:34 -0300

    fea(Bot pegando o emoji é o usuario da mensagem)

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ cliente_discord = discord.Client(intents=discord.Intents.all())
 
 canal_alvo_id = 1158343397279543327
 
+# Dicionário para armazenar as mensagens respondidas
+mensagens_respondidas = {}
+
 @cliente_discord.event
 async def on_ready():
     print('Logado como {0.user}'.format(cliente_discord))
@@ -14,6 +17,10 @@ async def on_ready():
 async def on_message(mensagem):
     # Verifica se a mensagem não foi enviada pelo próprio bot
     if mensagem.author == cliente_discord.user:
+        return
+
+    # Verifica se o bot já respondeu a esta mensagem
+    if mensagem.id in mensagens_respondidas:
         return
 
     if mensagem.channel.id == canal_alvo_id:
@@ -30,6 +37,13 @@ async def on_message(mensagem):
                     emojis = [char for char in texto if emoji.emoji_count(char)]
                     # Envia apenas o primeiro emoji encontrado
                     if emojis:
-                        await mensagem.channel.send(f'O emoji é {emojis[0]}')
+                        # Obtém o identificador e o nome do usuário
+                        id_usuario = mensagem.author.id
+                        nome_usuario = mensagem.author.name
+                        await mensagem.channel.send(f'O usuário {nome_usuario} com ID {id_usuario} enviou o emoji: {emojis[0]}')
+                        # Adiciona a mensagem ao dicionário de mensagens respondidas
+                        mensagens_respondidas[mensagem.id] = True
+                        return
+
 # Inicialização do cliente do Discord com o token de autenticação
 cliente_discord.run('')
