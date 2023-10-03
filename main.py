0bc6a5e438b5cc0c58634f9799bf2642f5437b26
jvras <jvras@cin.ufpe.br>
2023-10-03 08:16:24 -0300

    feat(limpeza é organização do codigo do bot)

--- main.py
+++ main.py
@@ -4,36 +4,48 @@ import pandas as pd
 import asyncio
 import datetime
 
+# Inicializando o cliente do Discord com todas as intenções
 cliente_discord = discord.Client(intents=discord.Intents.all())
 
-canal_alvo_id = 1158343397279543327 # chat teste 
-canal_planilha_id = 1158543934021173258 # chat teste2 
+
+canal_alvo_id = 1158343397279543327  # chat teste 
+canal_planilha_id = 1158543934021173258  # chat teste2 
 
 # DataFrame para armazenar os dados captados
 dados = pd.DataFrame(columns=['ID do Usuário', 'Nome do Usuário', 'Emoji', 'Data de Envio'])
 
 def salvar_dados():
-    # Exporta os dados para uma planilha Excel
-    dados.to_excel('dados.xlsx', index=False)
+    """
+    Função para salvar os dados em uma planilha Excel.
+    """
+    dados.to_excel('checkpoint.xlsx', index=False)
 
 async def alerta_checkpoint():
+    """
+    Função assíncrona para enviar um alerta de checkpoint no canal alvo todos os dias às 10h.
+    """
     await cliente_discord.wait_until_ready()
     canal = cliente_discord.get_channel(canal_alvo_id)
     while not cliente_discord.is_closed():
         agora = datetime.datetime.now()
-        if agora.hour == 10 and agora.minute == 0:  # defini para 10h todos os dias ele vai mandar um everyone para o canal de checkpoint
+        if agora.hour == 10 and agora.minute == 0:
             await canal.send("@everyone Lembre-se de responder ao #checkpoint!")
-            await asyncio.sleep(60)  # time para ele não ficar enviando varias mensagens
+            await asyncio.sleep(60)
         else:
-            await asyncio.sleep(1)  # so pra ele verificar novamente mais acho que isso vai ser desnecessario tbm (podia ser que nem o /checkpoint para marlos avisar todos os dias em determinada hora para fazer o checkpoint (lembra-los))
+            await asyncio.sleep(1)
 
 @cliente_discord.event
 async def on_ready():
+    """
+    Função para imprimir uma mensagem quando o bot estiver pronto.
+    """
     print('Logado como {0.user}'.format(cliente_discord))
 
 @cliente_discord.event
 async def on_message(mensagem):
-    # isso era pra verificar pq tava enviando a mensagem 2x mais acabou que foi culpa minha que tinha duas instancias do bot rodando kk então é inutil 
+    """
+    Função para lidar com mensagens recebidas.
+    """
     if mensagem.author == cliente_discord.user:
         return
 
@@ -51,11 +63,11 @@ async def on_message(mensagem):
                     emojis = [char for char in texto if emoji.emoji_count(char)]
                     # Envia apenas o primeiro emoji encontrado
                     if emojis:
-                        # Obtém o identificador e o nome do usuário
+                        # Obtem o identificador e o nome do usuário
                         id_usuario = mensagem.author.id
                         nome_usuario = mensagem.author.name
-                        data_envio = mensagem.created_at  # Obtém a data de envio da mensagem
-                        data_envio_sem_fuso_horario = data_envio.replace(tzinfo=None)  # Remove as informações de fuso horário
+                        data_envio = mensagem.created_at
+                        data_envio_sem_fuso_horario = data_envio.replace(tzinfo=None)
                         await mensagem.channel.send(f'O usuário {nome_usuario} com ID {id_usuario} enviou um emoji: {emojis[0]}')
                         
                         # Adiciona os dados ao DataFrame
@@ -63,13 +75,11 @@ async def on_message(mensagem):
                         
                         # Converte a coluna 'Data de Envio' para uma string
                         dados['Data de Envio'] = dados['Data de Envio'].astype(str)
-                        # Salva os dados na planilha
                         salvar_dados()
 
     elif mensagem.channel.id == canal_planilha_id and mensagem.content.strip() == '/checkpoint':
-        # Envia a planilha para o canal especificado quando o comando /checkpoint é recebido
         with open('checkpoint.xlsx', 'rb') as f:
-            await mensagem.channel.send("Aqui esta o checkpoint de hoje meu senhor:", file=discord.File(f, 'dados.xlsx'))
+            await mensagem.channel.send("Aqui esta o checkpoint de hoje:", file=discord.File(f, 'checkpoint.xlsx'))
 
 # Inicialização do cliente do Discord com o token de autenticação
 cliente_discord.run('')
