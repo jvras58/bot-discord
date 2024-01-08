f65ba870ee431c63ef44c0ed3e9c3ca940d3b134
jvras58 <jvras@cin.ufpe.br>
2024-01-08 12:51:39 -0300

    main2(responde no proprio canal)

--- bot/main2.py
+++ bot/main2.py
@@ -1,6 +1,7 @@
 from datetime import datetime, time
 import time
 
+
 import discord
 from config.conector_discord import ConectorDiscord
 from config.config import get_settings
@@ -52,14 +53,8 @@ async def comousar(interaction: discord.Interaction):
 # ------------------- Comandos de enviar everyone no canal INICIO ------------------- #
 @tree.command(name='horario_alerta', description='Define o horário do alerta')
 async def definir_alerta(interaction: discord.Interaction, horario: str):
-    # Responda à interação primeiro
-    await interaction.response.send_message(
-        'Definindo alerta...', ephemeral=True
-    )
     horario = datetime.strptime(horario, "%H:%M").time()
-    cliente_discord.alerta_checkpoint_horario = horario
-    dm_channel = await interaction.user.create_dm()
-    await dm_channel.send(f'Alerta definido para {cliente_discord.alerta_checkpoint_horario}.')
+    await interaction.response.send_message(f'Alerta definido para {cliente_discord.alerta_checkpoint_horario}.')
 
 @tree.command(name='offeveryone', description='Desativa menções a todos')
 async def offeveryone(interaction: discord.Interaction):
@@ -68,8 +63,6 @@ async def offeveryone(interaction: discord.Interaction):
         'Desativando menções a todos...', ephemeral=True
     )
     cliente_discord.enviar_everyone = False
-    dm_channel = await interaction.user.create_dm()
-    await dm_channel.send('Desativado menções a todos.')
 
 @tree.command(name='oneveryone', description='Ativa menções a todos')
 async def oneveryone(interaction: discord.Interaction):
@@ -78,8 +71,7 @@ async def oneveryone(interaction: discord.Interaction):
         'Ativando menções a todos...', ephemeral=True
     )
     cliente_discord.enviar_everyone = True
-    dm_channel = await interaction.user.create_dm()
-    await dm_channel.send('Ativado menções a todos.')
+
 # ------------------- Comandos de enviar everyone no canal FIM ------------------- #
 
 
@@ -93,16 +85,12 @@ async def oneveryone(interaction: discord.Interaction):
 
 
 # ------------------- Comandos de enviar dm no canal INICIO ------------------- #
+
 @tree.command(name='horario_verificar', description='Define o horário do verficar checkpoint')
 async def alerta_dm_horario(interaction: discord.Interaction, horario: str):
-    # Responda à interação primeiro
-    await interaction.response.send_message(
-        'Horario do alerta no horário sendo definido...', ephemeral=True
-    )
     horario = datetime.strptime(horario, "%H:%M").time()
     cliente_discord.verificar_checkpoint_horario = horario
-    dm_channel = await interaction.user.create_dm()
-    await dm_channel.send(f'Alerta definido para {cliente_discord.verificar_checkpoint_horario}.')
+    await interaction.response.send_message(f'Alerta definido para {cliente_discord.verificar_checkpoint_horario}.')
     
 @tree.command(name='offavisodm', description='Desativa aviso de mensagem direta')
 async def offavisodm(interaction: discord.Interaction):
@@ -111,8 +99,7 @@ async def offavisodm(interaction: discord.Interaction):
         'Desativando aviso de mensagem direta...', ephemeral=True
     )
     cliente_discord.enviar_dm = False
-    dm_channel = await interaction.user.create_dm()
-    await dm_channel.send('Desativado aviso de mensagem direta.')
+
 
 @tree.command(name='onavisodm', description='Ativa aviso de mensagem direta')
 async def onavisodm(interaction: discord.Interaction):
@@ -121,30 +108,19 @@ async def onavisodm(interaction: discord.Interaction):
         'Ativando aviso de mensagem direta...', ephemeral=True
     )
     cliente_discord.enviar_dm = True
-    dm_channel = await interaction.user.create_dm()
-    await dm_channel.send('Ativado aviso de mensagem direta.')
+
 
 #TODO: NECESSARIO TESTAR
 @tree.command(name='idignore', description='Adiciona um ID de usuário à lista de ignorados')
 async def idignore(interaction: discord.Interaction, id: str):
-    # Responda à interação primeiro
-    await interaction.response.send_message(
-        'Adicionando ID de usuário à lista de ignorados...', ephemeral=True
-    )
     cliente_discord.ids_ignorados = int(id)
-    dm_channel = await interaction.user.create_dm()
-    await dm_channel.send(f'ID de usuário {cliente_discord.ids_ignorados} adicionado à lista de ignorados.')
+    await interaction.response.send_message(f'ID de usuário {cliente_discord.ids_ignorados} adicionado à lista de ignorados.')
 
 #TODO: NECESSARIO TESTAR
 @tree.command(name='readicionarids', description='Remove um ID de usuário da lista de ignorados')
 async def readicionarids(interaction: discord.Interaction, id: str):
-    # Responda à interação primeiro
-    await interaction.response.send_message(
-        'Removendo ID de usuário da lista de ignorados...', ephemeral=True
-    )
     cliente_discord.ids_ignorados.remove(int(id))
-    dm_channel = await interaction.user.create_dm()
-    await dm_channel.send(f'ID de usuário {id} removido da lista de ignorados.')
+    await interaction.response.send_message(f'ID de usuário {id} removido da lista de ignorados.')
 # ------------------- Comandos de enviar dm no canal FIM ------------------- #
 
 
@@ -165,25 +141,16 @@ async def readicionarids(interaction: discord.Interaction, id: str):
 #FIXME: RECEBENDO O ID PELO COMANDO O PARAMETRO TEM QUE SER UM STR PARA SER TRANSFORMADO EM INT DEPOIS MESMO O canal_checkpoint_id sendo str averiguar depois o motivo....
 @tree.command(name='idcheckpoint', description='Define o ID do canal de checkpoint')
 async def idcheckpoint(interaction: discord.Interaction, id: str):
-    # Responda à interação primeiro
-    await interaction.response.send_message(
-        'Definindo ID do canal de checkpoint...', ephemeral=True
-    )
     cliente_discord.canal_checkpoint_id = int(id)
-    #print(f"ID do canal enviado definido para {id}.")
-    #print(f"ID do canal de checkpoint definido para {cliente_discord.canal_checkpoint_id}.")
-    dm_channel = await interaction.user.create_dm()
-    await dm_channel.send(f'ID do canal de checkpoint definido para {cliente_discord.canal_checkpoint_id}.')
+    # print(f"ID do canal enviado definido para {id}.")
+    # print(f"ID do canal de checkpoint definido para {cliente_discord.canal_checkpoint_id}.")
+    await interaction.response.send_message(f'ID do canal de checkpoint definido para {cliente_discord.canal_checkpoint_id}.')
+
 
 @tree.command(name='idplanilha', description='Define o ID do canal da planilha')
 async def idplanilha(interaction: discord.Interaction, id: str):
-    # Responda à interação primeiro
-    await interaction.response.send_message(
-        'Definindo ID do canal da planilha...', ephemeral=True
-    )
     cliente_discord.canal_planilha_id = int(id)
-    dm_channel = await interaction.user.create_dm()
-    await dm_channel.send(f'ID do canal da planilha definido para {cliente_discord.canal_planilha_id}.')
+    await interaction.response.send_message(f'ID do canal da planilha definido para {cliente_discord.canal_planilha_id}.')
 # ------------------- Comandos de configurações de canais FIM ------------------- #
 
 
